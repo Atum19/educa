3699c7a92789969ab01a63676058c7307a75a108
Stanislav Osmilovsky <osmstas@gmail.com>
2018-02-14 16:12:41 +0200

    added registration and enrolling modules

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -19,6 +19,7 @@ from braces.views import (
     # PermissionRequiredMixin,
 )
 from .forms import ModuleFormSet
+from students.forms import CourseEnrollForm
 from .models import Course, Module, Content, Subject
 
 
@@ -228,7 +229,7 @@ class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(CourseDetailView, self).get_context_data(**kwargs)
-    #     context['enroll_form'] = CourseEnrollForm(initial={'course':self.object})
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course':self.object})
+        return context
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
@@ -24,5 +26,9 @@ urlpatterns = [
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^course/', include('courses.urls')),
+    url(r'^students/', include('students.urls')),
     url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
